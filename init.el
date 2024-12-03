b95864c24423e1f703a87f6fc4276c89f703fed3
Alessandro-Zanella-OM <alessandro.zanella@openmind-tech.com>
2024-12-03 22:21:05 +0100

    progress

--- init.el
+++ init.el
@@ -1,2 +1,287 @@
-;; Don;t show the splash screen
+; Don't show the splash screen
 (setq inhibit-startup-message t)
+
+;; Flash when the bell rings
+(setq visible-bell t)
+
+;; Display numbers in every line
+(global-display-line-numbers-mode 1)
+
+(dolist (mode '(term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;;(load-theme 'deeper-blue t)
+;;(load-theme 'modus-vivendi t)
+;;(load-theme 'tango-dark)
+
+;; (set-face-attribute 'default nil :font "Fira Code Retina" :height 280)
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+
+(menu-bar-mode -1)            ; Disable the menu bar
+
+;; Make ESC quit prompts
+;;(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(setq show-trailing-whitespace t)
+
+;; Visualizza il numero della colonna nella barra
+(setq column-number-mode t)
+
+;; Prosegue la ricerca incrementale partendo dall'inizio del file
+(setq isearch-wrap-pause 'no-ding)
+
+;; Al termine di una ricerca i valori trovati rimangono evidenziati
+;; (setq lazy-highlight-cleanup nil)
+
+(setq lazy-highlight t)
+
+;; Evidenzia subito tutte le occorrenze trovate
+(setq lazy-highlight-max-at-a-time nil)
+
+;; imposta il delay a zero per evidenziare subito le occorrenze trovate
+(setq lazy-highlight-initial-delay 0)
+(setq isearch-lazy-count t)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+			 (cons "gnu-devel" "https://elpa.gnu.org/devel/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package nerd-icons)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 30)))
+
+(setq doom-modeline-icon t)
+
+(defun ddate () (interactive) (insert (format-time-string "(%H:%M)" (current-time))))
+
+;;(setq org-agenda-files '("c:/Users/Alessandro.Zanella/OneDrive - MuM - OM/Notes/"))
+(setq org-agenda-files (directory-files-recursively "c:/Users/Alessandro.Zanella/OneDrive - MuM - OM/Notes/" "\\.org$"))
+
+(setq backup-directory-alist `(("." . "~/.emacsBackup")))
+
+(setq-default cursor-type 'bar)
+
+;; Quando si entra in una folder with dired
+;; uccide il buffer della folder appena visitata
+(setq dired-kill-when-opening-new-dired-buffer t)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package hydra)
+(use-package org-fc
+  :load-path "C:/WorkDir/emacs/org-fc"
+  :custom (org-fc-directories '("c:/Users/Alessandro.Zanella/OneDrive - MuM - OM/Notes/FlashCards"))
+  :config
+  (require 'org-fc-hydra))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-monokai-pro t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(use-package evil
+ :init
+ (setq evil-want-integration t)
+ (setq evil-want-keybinding nil)
+ (setq evil-want-C-u-scroll t)
+ (setq evil-want-C-i-jump nil))
+
+(evil-mode)
+
+ ;Use visual line motions even outside of visual-line-mode buffers
+ ;(evil-global-set-key 'motion "j" 'evil-next-visual-line)
+ ;(evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+ ;(evil-set-initial-state 'messages-buffer-mode 'normal)
+ ;(evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+ :after evil
+ :config
+ (evil-collection-init))
+
+(evil-escape-mode)
+(setq-default evil-escape-key-sequence "jk")
+(setq-default evil-escape-delay 0.2)
+;;
+;;(use-package evil-org
+;;  :ensure t
+;;  :after org
+;;  :hook (org-mode . (lambda () evil-org-mode))
+;;  :config
+;;  (require 'evil-org-agenda)
+;;  (evil-org-agenda-set-keys))
+
+;; Example configuration for Consult
+(use-package consult
+  ;; Replace bindings. Lazily loaded by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
+  )
+
+  (setq org-file-apps
+	'(("\\.mp3\\'" . "cmdmp3win.exe %s" )))
+        ;;'(("\\.mp3\\'" . "C:\Program Files\WindowsApps\Resolute.foobar2000_1.6.180.0_x86__cg7j1awqsza28\foobar2000.exe %s" )))
+
+;; Intallazione dei package per gestire python
+;;(use-package elpy)
+;;(elpy-enable)
+
+;;(use-package flycheck)
+;; Enable Flycheck
+;;(when (require 'flycheck nil t)
+;;  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+;;  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; (use-package jedi)
+;;(use-package py-autopep8)
+;; (use-package blacken)
+;;(use-package )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(powershell evil-escape hydra org-drill use-package rainbow-delimiters py-autopep8 jedi flycheck evil-org evil-collection elpy doom-themes doom-modeline consult blacken)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
